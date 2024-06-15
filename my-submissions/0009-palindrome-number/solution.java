8fd9ac69f162a6e24d946ef295424cfb8f92c953
Saumya Gupta <155184666+cosmicveil1@users.noreply.github.com>
2024-06-15 11:30:41 +0000

    Sync LeetCode submission Runtime - 4 ms (100.00%), Memory - 43.7 MB (99.99%)

---
+++ my-submissions/0009-palindrome-number/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false; // negative numbers are not palindromes
+        int original = x; // store original value
+        int res = 0;
+        while (x > 0) {
+            int rem = x % 10;
+            res = res * 10 + rem;
+            x = x / 10;
+        }
+        return res == original; // compare with original value
+    }
+}
